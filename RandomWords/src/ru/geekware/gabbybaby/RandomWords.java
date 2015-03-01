64d4a202e5343bf6a2d2de622f93dccc53a5e023
Dmitry Khokhlov <khokhlov@targetix.net>
2015-03-01 12:33:06 +0300

    Project: создан проект в IntelliJ IDEA

---
+++ RandomWords/src/ru/geekware/gabbybaby/RandomWords.java
@@ -0,0 +1,15 @@
+package ru.geekware.gabbybaby;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RandomWords extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
