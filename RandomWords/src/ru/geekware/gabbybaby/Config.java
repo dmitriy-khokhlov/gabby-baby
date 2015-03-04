1046bb5caa187dbe4de4f0e0f02cab74161a088f
Dmitry Khokhlov <khokhlov@targetix.net>
2015-03-04 08:51:10 +0300

    RandomWords: изменены задержки

--- RandomWords/src/ru/geekware/gabbybaby/Config.java
+++ RandomWords/src/ru/geekware/gabbybaby/Config.java
@@ -8,8 +8,8 @@ public class Config {
     public static String[] defaultWords =
             new String[] { "МАМА", "ПАПА", "БАБА", "ДЕДА", "ВАНЯ" };
 
-    public static int wordDisplayTimeMillis = 3000;
-    public static int pauseBetweenWordsMillis = 300;
+    public static int wordDisplayTimeMillis = 2000;
+    public static int pauseBetweenWordsMillis = 200;
 
     public static int maxWordsInSequence = 5;
 }
